fc84c635e573b8a8cc95f7f026796ae5c003cc4e
alexsotn <64767365+alexsotn@users.noreply.github.com>
2020-10-26 13:40:24 +0300

    Add files via upload

---
+++ Poker_Alex_Sotnikov/GameResetInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poker_Alex_Sotnikov {
+	interface GameResetInterface {
+		void Unsubscribe(GameControl game);
+		void Subscribe(GameControl game);
+		void ReInitialize();
+	}
+}
---
+++ Poker_Alex_Sotnikov/NewGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Poker_Alex_Sotnikov {
+
+	public partial class NewGame : Form {
+		public event EventHandler NewGameInit;
+
+		public NewGame()
+		{
+			InitializeComponent();
+		}
+
+		public void OnNewGameInit(GameInitArgs e)
+		{
+			EventHandler newgameinit = NewGameInit;
+			if (newgameinit != null) newgameinit(this, e);
+		}
+
+		private void buttonNewGame_Click(object sender, EventArgs e)
+		{
+			Close();
+			OnNewGameInit(new GameInitArgs(2, (int)nudInitStack.Value, tbName.Text));
+		}
+	}
+}
